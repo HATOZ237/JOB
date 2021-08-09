3ec0165adf10d9dddb64e6172840f7dbdfbd5e23
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2021-08-09 13:46:00 -0400

    Update cma-svc.py

--- cma-svc.py
+++ cma-svc.py
@@ -71,7 +71,7 @@ def main():
         while value[0] > 1.9:
             value[0] = value[0]/2
         model = SVC(C = 10**(3*value[0]), gamma=10**(-3*value[1]), kernel=kernel[round(abs(value[2]*4))%3])
-        scores = cross_val_score(model, x_train, y_train, cv = 4, n_jobs=-1)
+        scores = cross_val_score(model, x_train, y_train, cv = 3, n_jobs=-1)
             #print(value)
         return scores.mean(), #Add a comma even if there is only one return value
 
