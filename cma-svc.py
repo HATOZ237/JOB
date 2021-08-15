b3d210886a742ab4646147df223c975b2320b7d3
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2021-08-15 15:01:23 -0400

    Update cma-svc.py

--- cma-svc.py
+++ cma-svc.py
@@ -85,7 +85,7 @@ def score(value):
 
 #calcul des performances
 def main():
-    for total in [1, 5, 10, 25, 50, 75, 100, 125, 150, 175, 200]:
+    for total in [ 100, 125, 150, 175, 200]:
         ea_results = {}
         cma_results = {}
 
