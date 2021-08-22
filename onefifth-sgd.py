2a1c195724b4e6e034099d2f4abd8228aafb6b3d
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2021-08-22 16:22:06 -0400

    Update onefifth-sgd.py

--- onefifth-sgd.py
+++ onefifth-sgd.py
@@ -148,5 +148,5 @@ if __name__ == "__main__":
                 process[x].join()
         
             one_results[names[i]] = {"max_test_score": np.max(test_liste),"max_train_score":np.max(train_liste), 'test_score": np.mean(test_liste), "std_test":np.std(test_liste),"train_score":np.mean(train_liste) ,'std_train':np.std(test_liste) , "Time":np.mean(time_liste)}
-        pd.DataFrame(one_results).to_csv(f"ONEFIFTH-SGD-{total}")
+        pd.DataFrame(one_results).to_csv(f"ONEFIFTHS-SGD-{total}")
         print(f"{total} essais terminé")
