efedafd9181bd21021108d689077110cba4d391d
TOUKAM ZANJIO HAROLD WILSON <harold.toukam-zanjio.1@ulaval.ca>
2021-08-24 23:51:01 -0400

    petit changement 2

--- onefifth-svc.py
+++ onefifth-svc.py
@@ -119,12 +119,12 @@ if __name__ == "__main__":
     best_score = [0]*4
     best2 = [0]*4
     process = [0 for _ in range(turn)]
-    for total in range(200):
+    for total in range(100):
         print(f"{total + 1} essais ")
         for i in range(len(datasets)):
             x_train, x_test, y_train, y_test = train_test_split(data_s[i], target_s[i], shuffle=False, train_size=0.75)
             x_train, x_test = StandardScaler().fit_transform(x_train), StandardScaler().fit_transform(x_test)
-            best, time1 = main(100)
+            best, time1 = main(200)
             train_score = evalOneMax(best)[0]
             if best_score[i] < train_score:
                 best_score[i] = train_score
