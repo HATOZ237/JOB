b6c0c08ded1a897742996d35a95eaa2308c22c16
TOUKAM ZANJIO HAROLD WILSON <harold.toukam-zanjio.1@ulaval.ca>
2021-08-28 14:15:34 -0400

    petit changement one sgd

--- onefifth-sgd.py
+++ onefifth-sgd.py
@@ -19,9 +19,9 @@ kernel = ["linear", "rbf", "poly", "sigmoid"]
 def evalOneMax(value):
     loss = ['hinge', 'log', 'perceptron', 'modified_huber', "squared_hinge"]
     learning_rate = ["constant", 'optimal', 'adaptive', 'invscaling']
-    model = SGDClassifier(n_jobs=-1, eta0=0.00001, loss=loss[round(abs(value[0] * 6)) % 4],
-                          learning_rate=learning_rate[round(abs(value[1] * 5)) % 3], l1_ratio=abs(value[2] % 1),
-                          alpha=10 ** (-3 * value[3]))
+    model = SGDClassifier(n_jobs=-1, eta0=0.00001, loss=loss[round(abs(value[0] * 4)) % 4],
+                          learning_rate=learning_rate[round(abs(value[1] * 3)) % 3], l1_ratio=abs(value[2] % 1),
+                          alpha=10 ** (-4 * value[3]))
     scores = cross_val_score(model, x_train, y_train, cv=3, n_jobs=-1)
     return scores.mean(),  # Add a comma even if there is only one return value
 
@@ -29,9 +29,9 @@ def evalOneMax(value):
 def score(value):
     loss = ['hinge', 'log', 'perceptron', 'modified_huber', "squared_hinge"]
     learning_rate = ["constant", 'optimal', 'adaptive', 'invscaling']
-    model = SGDClassifier(n_jobs=-1, eta0=0.00001, loss=loss[round(abs(value[0] * 6)) % 4],
-                          learning_rate=learning_rate[round(abs(value[1] * 5)) % 3], l1_ratio=abs(value[2] % 1),
-                          alpha=10 ** (-3 * value[3]))
+    model = SGDClassifier(n_jobs=-1, eta0=0.00001, loss=loss[round(abs(value[0] * 4)) % 4],
+                          learning_rate=learning_rate[round(abs(value[1] * 3)) % 3], l1_ratio=abs(value[2] % 1),
+                          alpha=10 ** (-4 * value[3]))
     model.fit(x_train, y_train)
     return model.score(x_test, y_test)
 
@@ -64,7 +64,7 @@ def main(ngen):
     logbook.header = "gen", "fitness", 'loss', 'alpha', 'l1_ratio', "learning_rate"
 
     # interval = (-3,7)
-    func = [random(), random(), random(), gauss(0, 0.5)]
+    func = [random(), random(), random(), random()]
     mu = func
     sigma = 0.5
     alpha = 2.0 ** (1.0 / IND_SIZE)
@@ -84,9 +84,9 @@ def main(ngen):
             best, worst = worst, best
         else:
             sigma = sigma * alpha ** (-0.25)
-    logbook.record(gen=g, fitness=best.fitness.values[0], loss=loss[round(abs(best[0] * 6)) % 5],
-                   learning_rate=learning_rate[round(abs(best[1] * 5)) % 4], l1_ratio=abs(best[2] % 1),
-                   alpha=10 ** (-3 * best[3]), score=score(best))
+    logbook.record(gen=g, fitness=best.fitness.values[0], loss=loss[round(abs(best[0] * 4)) % 4],
+                   learning_rate=learning_rate[round(abs(best[1] * 3)) % 3], l1_ratio=abs(best[2] % 1),
+                   alpha=10 ** (-4 * best[3]), score=score(best))
     if best_score < best.fitness.values[0]:
         best_score = best.fitness.values[0]
         save = best
@@ -98,7 +98,7 @@ def main(ngen):
 
 
 if __name__ == "__main__":
-    seed(100000)
+
     np.random.seed(100000)
     datasets = [load_breast_cancer(), load_digits(), load_iris(), load_wine()]  # , load_linnerud
     names = ['load_breast_cancer', 'load_digits', 'load_iris', "load_wine"]  # 'load_linnerud'
@@ -115,7 +115,7 @@ if __name__ == "__main__":
         target_s[i] = dataset.target
         pocket = list(zip(data_s[i], target_s[i]))
         # print(pocket)
-        shuffle(pocket)
+        np.random.shuffle(pocket)
         data_s[i] = [x[0] for x in pocket]
         target_s[i] = [x[1] for x in pocket]
         # feature_names[i] = dataset.feature_names
@@ -129,11 +129,12 @@ if __name__ == "__main__":
     best2 = [0] * 4
     process = [0 for _ in range(turn)]
     for k in range(10):
+        np.random.seed(randint(1, 100000))
         for total in range(10):
             print(f"{total + 1} essais ")
             for i in range(len(datasets)):
                 x_train, x_test, y_train, y_test = train_test_split(data_s[i], target_s[i], shuffle=False,
-                                                                    train_size=0.75)
+                                                                    train_size=0.75, random_state=0)
                 x_train, x_test = StandardScaler().fit_transform(x_train), StandardScaler().fit_transform(x_test)
                 best, time1 = main(200)
                 train_score = evalOneMax(best)[0]
