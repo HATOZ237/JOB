330ed898c871a34ce50575cb22ad6702580f11dc
TOUKAM ZANJIO HAROLD WILSON <harold.toukam-zanjio.1@ulaval.ca>
2021-08-28 00:05:39 -0400

    petit changement one svc

--- cma-sgd.py
+++ cma-sgd.py
@@ -18,7 +18,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 import pickle
 
-seed(100000)
+
 np.random.seed(100000)
 datasets = [load_breast_cancer(), load_digits(), load_iris(), load_wine()]  # , load_linnerud
 names = ['load_breast_cancer', 'load_digits', 'load_iris', "load_wine"]  # 'load_linnerud'
@@ -32,14 +32,14 @@ for i, dataset in enumerate(datasets):
     target_s[i] = dataset.target
     pocket = list(zip(data_s[i], target_s[i]))
     # print(pocket)
-    shuffle(pocket)
+    np.random.shuffle(pocket)
     data_s[i] = [x[0] for x in pocket]
     target_s[i] = [x[1] for x in pocket]
     feature_names[i] = dataset.feature_names
     description[i] = dataset.DESCR
     target_names[i] = dataset.target_names
 
-np.random.seed(1000)
+
 n_iter = 0
 func_seq = [lambda: random(), lambda: random(), lambda: random(), lambda: random()]
 loss = ['hinge', 'log', 'perceptron', 'modified_huber', "squared_hinge"]
@@ -77,9 +77,9 @@ def evalOneMax(value):
 def evalOne(value):
     loss = ['hinge', 'log', 'perceptron', 'modified_huber', "squared_hinge"]
     learning_rate = ["constant", 'optimal', 'adaptive', 'invscaling']
-    model = SGDClassifier(n_jobs=-1, eta0=0.00001, loss=loss[round(abs(value[0] * 6)) % 4],
-                          learning_rate=learning_rate[round(abs(value[1] * 5)) % 3], l1_ratio=abs(value[2] % 1),
-                          alpha=10 ** (-4 * value[3]))
+    model = SGDClassifier(n_jobs=-1, eta0=0.00001, loss=loss[round(abs(value[0] * 4)) % 4],
+                          learning_rate=learning_rate[round(abs(value[1] * 3)) % 3], l1_ratio=abs(value[2] % 1),
+                          alpha=10 ** (-3 * value[3] - 1))
     scores = cross_val_score(model, x_train, y_train, cv=3, n_jobs=-1)
     return scores.mean(),  # Add a comma even if there is only one return value
 
@@ -87,9 +87,9 @@ def evalOne(value):
 def score(value):
     loss = ['hinge', 'log', 'perceptron', 'modified_huber', "squared_hinge"]
     learning_rate = ["constant", 'optimal', 'adaptive', 'invscaling']
-    model = SGDClassifier(n_jobs=-1, eta0=0.00001, loss=loss[round(abs(value[0] * 6)) % 4],
-                          learning_rate=learning_rate[round(abs(value[1] * 5)) % 3], l1_ratio=abs(value[2] % 1),
-                          alpha=10 ** (-4 * value[3]))
+    model = SGDClassifier(n_jobs=-1, eta0=0.00001, loss=loss[round(abs(value[0] * 4)) % 4],
+                          learning_rate=learning_rate[round(abs(value[1] * 3)) % 3], l1_ratio=abs(value[2] % 1),
+                          alpha=10 ** (-3 * value[3] - 1))
     model.fit(x_train, y_train)
     return model.score(x_test, y_test)
 tab = {}
@@ -105,7 +105,7 @@ def main(idi):
     for k in range(10):
         for i in range(len(datasets)):
             global x_train, x_test, y_train, y_test
-            x_train, x_test, y_train, y_test = train_test_split(data_s[i], target_s[i], shuffle=False, train_size=0.75)
+            x_train, x_test, y_train, y_test = train_test_split(data_s[i], target_s[i], shuffle=False, train_size=0.75, random_state=0)
             x_train, x_test = StandardScaler().fit_transform(x_train), StandardScaler().fit_transform(x_test)
             toolbox.register("evaluate", evalOneMax)
             #pool = multiprocessing.Pool()
@@ -140,9 +140,9 @@ def main(idi):
             train_liste = list(map(f, scores))
             if best_score[i] < max(train_liste):
                 best_score[i] = max(train_liste)
-            cma_results[names[i]] = {'loss': loss[round(abs(best2[0] * 6)) % 4],
-                                     "learning_rate": learning_rate[round(best2[1] % 3)], 'l1_ratio': abs(best2[2] % 1),
-                                     "alpha": 10 ** (-3 * best2[3]),
+            cma_results[names[i]] = {'loss': loss[round(abs(best2[0] * 4)) % 4],
+                                     "learning_rate": learning_rate[round(abs(value[1] * 3)) % 3], 'l1_ratio': abs(best2[2] % 1),
+                                     "alpha": 10 ** (-3 * best2[3] - 1),
                                      "max_train_score": best_score[i], 'test_score': score(best2),
                                      "train_score": np.mean(train_liste), "std_train": np.std(train_liste),
                                      "Time": times[i]}
@@ -155,7 +155,7 @@ def main(idi):
 if __name__ == "__main__":
     for id in range(10):
         print("------------------- Tour  : " + str(id) + " ------------------------")
-        seed(randint(1, 100000))
+        np.random.seed(randint(1, 100000))
         main(id)
     file_name = "CMA-TAB-SGD"
     outfile = open(file_name, "wb")
