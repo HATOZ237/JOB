2407df9edbfe3c26097cb439bf0f9286575df019
TOUKAM ZANJIO HAROLD WILSON <harold.toukam-zanjio.1@ulaval.ca>
2021-08-29 09:48:11 -0400

    petit changement cma one rf

--- onefifth-kn.py
+++ onefifth-kn.py
@@ -27,7 +27,7 @@ from statistics import *
 from deap import cma
 import multiprocessing
 from multiprocessing import Process
-
+import pickle
 
 
     
@@ -65,85 +65,109 @@ toolbox.register("evaluate", evalOneMax)
 
 
 
-def main(ngen, id):
+def main(id):
     IND_SIZE = 10
-    
-    #random.seed(64)
-    
-    #logbook = tools.Logbook()
-    #logbook.header = "gen", "fitness", 'loss', 'alpha', 'l1_ratio',"learning_rate", "score"
+    start = time()
 
-    #interval = (-3,7)
+    # random.seed(64)
+
+    logbook = tools.Logbook()
+    logbook.header = "gen", "fitness", "id"
+
+    # interval = (-3,7)
     func = [random(), random(), random()]
     mu = func
     sigma = 0.5
-    alpha = 2.0**(1.0/IND_SIZE)
+    alpha = 2.0 ** (1.0 / IND_SIZE)
 
     best = creator.Individual(mu)
     best.fitness.values = toolbox.evaluate(best)
     worst = creator.Individual(func)
-    #print(worst)
-
-    NGEN = ngen
+    # print(worst)
+    best_score = 0
+    save = 0
+    NGEN = 200
     for g in range(NGEN):
-        toolbox.update(worst, best, sigma) 
+        toolbox.update(worst, best, sigma)
         worst.fitness.values = toolbox.evaluate(worst)
         if best.fitness <= worst.fitness:
             sigma = sigma * alpha
             best, worst = worst, best
         else:
-            sigma = sigma * alpha**(-0.25) 
-        #logbook.record(gen=g, fitness=best.fitness.values[0], loss=loss[round(abs(best[0]*6))%5], learning_rate=learning_rate[round(abs(best[1]*5))%4], l1_ratio=abs(best[2]%1), alpha=10**(-3*best[3]), score=score(best))
-        #print(logbook.stream)
-    #print("Fin de l'algorithme en "+ str(n_iter)+" tours")
-    #start = time()-start
-    global train_liste, test_liste, time_liste
-    train_liste[id] = evalOneMax(best)[0]
-    test_liste[id] = score(best)
-    #time_liste[id] = time() - start
-    #return best, start
-    
+            sigma = sigma * alpha ** (-0.25)
+    logbook.record(gen=g, fitness=best.fitness.values[0], id = id)
+    print(logbook.stream)
+    # print("Fin de l'algorithme en "+ str(n_iter)+" tours")
+    if best_score < best.fitness.values[0]:
+        best_score = best.fitness.values[0]
+        save = best
+
+    # print("Fin de l'algorithme en "+ str(n_iter)+" tours")
+    start = time() - start
+
+    scores[id] = best_score
+
 if __name__ == "__main__":
     np.random.seed(100000)
-    datasets = [load_breast_cancer(), load_digits(), load_iris(), load_wine()]#, load_linnerud
-    names = ['load_breast_cancer', 'load_digits', 'load_iris', "load_wine"]# 'load_linnerud'
+    datasets = [load_breast_cancer(), load_digits(), load_iris(), load_wine()]  # , load_linnerud
+    names = ['load_breast_cancer', 'load_digits', 'load_iris', "load_wine"]  # 'load_linnerud'
     data_s = [None for i in range(len(datasets))]
     target_s = [None for i in range(len(datasets))]
     target_names = [None for i in range(len(datasets))]
     feature_names = [None for i in range(len(datasets))]
-    description  = [None for i in range(len(datasets))]
+    description = [None for i in range(len(datasets))]
+    tab = {}
+    for i in range(len(names)):
+        tab[names[i]] = [[0 for _ in range(10)] for k in range(10)]
     for i, dataset in enumerate(datasets):
-        data_s[i] = dataset.data
+        data_s[i] = StandardScaler().fit_transform(dataset.data)
         target_s[i] = dataset.target
         pocket = list(zip(data_s[i], target_s[i]))
-       # print(pocket)
+        # print(pocket)
         np.random.shuffle(pocket)
         data_s[i] = [x[0] for x in pocket]
         target_s[i] = [x[1] for x in pocket]
-        feature_names[i] = dataset.feature_names
-        description[i] = dataset.DESCR
-        target_names[i] = dataset.target_names
-    turn  = 10
-    x_train, x_test, y_train, y_test = 0,0,0,0
+        # feature_names[i] = dataset.feature_names
+        # description[i] = dataset.DESCR
+        # target_names[i] = dataset.target_names
+    turn = 10
+    x_train, x_test, y_train, y_test = 0, 0, 0, 0
     one_results = {}
-    train_liste = multiprocessing.Array('d', turn)
-    test_liste = multiprocessing.Array('d', turn)
-    start = 0
+    start = [0] * 4
+    best_score = [0] * 4
+    best2 = [0] * 4
     process = [0 for _ in range(turn)]
-    for total in [10, 50, 100, 250, 500, 750, 1000,  1250, 1500, 1750, 2000, 2500]:
-        print(f"{total} essais demarré")
+
+    for k in range(10):
+        scores = multiprocessing.Array('d', 10)
+        np.random.seed(randint(1, 100000))
+        print(f"{k + 1} essais ")
         for i in range(len(datasets)):
-            x_train, x_test, y_train, y_test = train_test_split(data_s[i], target_s[i], shuffle=False, train_size=0.75)
-            x_train, x_test = StandardScaler().fit_transform(x_train), StandardScaler().fit_transform(x_test)
+            x_train, x_test, y_train, y_test = train_test_split(data_s[i], target_s[i], shuffle=False,
+                                                                train_size=0.75, random_state=0)
+            # x_train, x_test = StandardScaler().fit_transform(x_train), StandardScaler().fit_transform(x_test)
             for x in range(turn):
-                process[x] = Process(target = main, args = (total,x))
+                process[x] = Process(target = main, args = (x,))
             start = time()
             for x in range(turn):
                 process[x].start()
             for x in range(turn):
                 process[x].join()
-            start = time() - start
-        
-            one_results[names[i]] = {"max_test_score": np.max(test_liste), "max_train_score": np.max(train_liste),"test_score": np.mean(test_liste), "std_test":np.std(test_liste),"train_score":np.mean(train_liste) ,'std_train':np.std(test_liste) , "Time":start/turn}
-        pd.DataFrame(one_results).to_csv(f"ONEFIFTHS-KN-{total}")
-        print(f"{total} essais terminé")
+            #best, time1 = main(200)
+
+            """if best_score[i] < best:
+                best_score[i] = best
+                best2[i] = best"""
+            # start[i] = start[i] + time1
+            tab[names[i]][k] = list(sorted(scores))
+            """one_results[names[i]] = {"kernel": kernel[round(best2[i][2] % 3)], "C": 10 ** (-4 * best2[i][0] + 4),
+                                     'gamma': 10 ** (-7.5 * abs(best2[i][1]) + 2.5),
+                                     "test_score": score(best2[i]),
+                                     "train_score": best_score[i],
+                                     "Time": start[i]}
+        pd.DataFrame(one_results).to_csv(f"ONEFIFTH-SVC-{(total + 1) * 25}")"""
+file_name = "ONE-TAB-KN"
+outfile = open(file_name, "wb")
+print(tab)
+pickle.dump(tab, outfile)
+outfile.close()
